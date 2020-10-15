bf0e07b76c47f2d4bfe14f7e5edfcc980236942b
luisgava <63642035+luisgava@users.noreply.github.com>
2020-10-15 20:53:17 +0200

    L

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,6 +1,8 @@
 package controlador;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
 import java.util.Properties;
 import java.util.Set;
 
@@ -8,9 +10,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Controlador {
 	
+	/**
+	 * Deserializa el string en formato JSON en un objeto JAVA
+	 * @param pathname
+	 * @return
+	 */
 	public Prediccion fromFileToObject(String pathname) {
 		Prediccion data = null;
-
+		URL enlace = new URL("http://worldweather.wmo.int/es/json/" + numCiudad +"_es.xml");
 		try {
 			ObjectMapper mapper = new ObjectMapper();
 			data = mapper.readValue(new File(pathname), Prediccion.class);
@@ -23,6 +30,7 @@ public class Controlador {
 	
 	//obtener lista de ciudades del fichero de propiedades
 	Properties configuracion = new Properties();	
+	
 	configuracion.load(new FileReader("src/resources/config.properties"));
 	Set<String> ciudades = configuracion.stringPropertyNames();
 	//leer fichero de propiedades
